aeba9f127dcef3dcb40fec43aeb36f58ef94b89a
Kevin Läufer <laeufer@cs.berkeley.edu>
2022-06-16 17:24:09 -0700

    lshift_reg: add sanity checks

--- test/bugfix/BugfixerTests.scala
+++ test/bugfix/BugfixerTests.scala
@@ -13,6 +13,26 @@ abstract class BugFixerSpec extends AnyFlatSpec {
   val VerboseConfig = DefaultConfig.copy(verbose = true, debugSolver = true)
 }
 
+class BugfixerShiftRegTests extends BugFixerSpec {
+  behavior.of("Bugfixer on ShiftReg")
+  private val Dir = CirFixDir / "lshift_reg"
+
+  it should "find that no repair is necessary for lshift_reg" in {
+    val res = Bugfixer.repair(Dir / "lshift_reg.btor", Dir / "orig_tb.csv", DefaultConfig)
+    assert(res.noRepairNecessary, res.toString)
+  }
+
+  it should "fail to fix lshift_reg_wadden_buggy1 with original testbench" in {
+    val res = Bugfixer.repair(Dir / "lshift_reg_wadden_buggy1.btor", Dir / "orig_tb.csv", DefaultConfig)
+    assert(res.cannotRepair, res.toString) // cannot be repaired since we do not have the right template yet
+  }
+
+  it should "fail to fix lshift_reg_wadden_buggy2 with original testbench" in {
+    val res = Bugfixer.repair(Dir / "lshift_reg_wadden_buggy2.btor", Dir / "orig_tb.csv", DefaultConfig)
+    assert(res.cannotRepair, res.toString) // cannot be repaired since we do not have the right template yet
+  }
+}
+
 class BugfixerFsmFullTests extends BugFixerSpec {
   behavior.of("Bugfixer on FsmFull")
   private val Dir = CirFixDir / "fsm_full"
