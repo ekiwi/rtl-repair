35bb462ab004a9b5df346905fad98727783cf3cd
Kevin Läufer <laeufer@cs.berkeley.edu>
2022-06-17 16:20:35 -0700

    move literal replacer to its own file

--- rtlfix.py
+++ rtlfix.py
@@ -4,12 +4,12 @@
 # author: Kevin Laeufer <laeufer@cs.berkeley.edu>
 
 import argparse
-from rtlfix import RepairTemplate, Namespace
 from pathlib import Path
 from pyverilog.vparser.parser import parse
-from pyverilog.vparser.ast import Source, IntConst
+from pyverilog.vparser.ast import Source
 from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
-import pyverilog.vparser.ast as vast
+
+from rtlfix.literalrepl import replace_literals
 
 _script_dir = Path(__file__).parent.resolve()
 _parser_tmp_dir = _script_dir / ".pyverilog"
@@ -42,38 +42,6 @@ def serialize(ast: Source) -> str:
     codegen = ASTCodeGenerator()
     source = codegen.visit(ast)
     return source
-
-
-def replace_literals(ast: Source):
-    namespace = Namespace(ast)
-    repl = LiteralReplacer()
-    repl.apply(namespace, ast)
-
-
-_bases = {'b': 2, 'o': 8, 'h': 16, 'd': 10}
-
-
-def parse_verilog_int_literal(value: str) -> (int, int):
-    assert "'" in value, f"unsupported integer constant format: {value}"
-    parts = value.split("'")
-    width = int(parts[0])
-    prefix = parts[1][0]
-    if prefix in _bases:
-        value = int(parts[1][1:], _bases[prefix])
-    else:
-        value = int(parts[1])
-    return value, width
-
-
-class LiteralReplacer(RepairTemplate):
-    def __init__(self):
-        super().__init__(name="literal")
-
-    def visit_IntConst(self, node: IntConst):
-        value, bits = parse_verilog_int_literal(node.value)
-        new_const = vast.Identifier(self.make_synth_var(bits))
-        choice = self.make_change(new_const, node)
-        return choice
 
 
 if __name__ == '__main__':
---
+++ rtlfix/literalrepl.py
@@ -0,0 +1,39 @@
+# Copyright 2022 The Regents of the University of California
+# released under BSD 3-Clause License
+# author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+
+from rtlfix import RepairTemplate, Namespace
+import pyverilog.vparser.ast as vast
+
+
+def replace_literals(ast: vast.Source):
+    namespace = Namespace(ast)
+    repl = LiteralReplacer()
+    repl.apply(namespace, ast)
+
+
+_bases = {'b': 2, 'o': 8, 'h': 16, 'd': 10}
+
+
+def parse_verilog_int_literal(value: str) -> (int, int):
+    assert "'" in value, f"unsupported integer constant format: {value}"
+    parts = value.split("'")
+    width = int(parts[0])
+    prefix = parts[1][0]
+    if prefix in _bases:
+        value = int(parts[1][1:], _bases[prefix])
+    else:
+        value = int(parts[1])
+    return value, width
+
+
+class LiteralReplacer(RepairTemplate):
+    def __init__(self):
+        super().__init__(name="literal")
+
+    def visit_IntConst(self, node: vast.IntConst):
+        value, bits = parse_verilog_int_literal(node.value)
+        new_const = vast.Identifier(self.make_synth_var(bits))
+        choice = self.make_change(new_const, node)
+        return choice
+
