3e88886fe67c42c8c2ed222e46dd2410a4b04f91
Kevin Läufer <laeufer@cs.berkeley.edu>
2022-06-17 16:41:57 -0700

    convert to btor

--- rtlfix.py
+++ rtlfix.py
@@ -4,6 +4,8 @@
 # author: Kevin Laeufer <laeufer@cs.berkeley.edu>
 
 import argparse
+import os
+import subprocess
 from pathlib import Path
 from pyverilog.vparser.parser import parse
 from pyverilog.vparser.ast import Source
@@ -18,15 +20,40 @@ _parser_tmp_dir = _script_dir / ".pyverilog"
 def parse_args():
     parser = argparse.ArgumentParser(description='Repair Verilog file')
     parser.add_argument('--source', dest='source', help='Verilog source file', required=True)
+    parser.add_argument('--working-dir', dest='working_dir', help='Working directory, files might be overwritten!',
+                        required=True)
     args = parser.parse_args()
-    return Path(args.source)
+    return Path(args.source), Path(args.working_dir)
+
+
+def create_working_dir(working_dir: Path):
+    if not os.path.exists(working_dir):
+        os.mkdir(working_dir)
+
+
+def to_btor(filename: Path):
+    cwd = filename.parent
+    assert cwd.exists(), f"directory {cwd} does not exist"
+    r = subprocess.run(["yosys", "-version"], check=False, stdout=subprocess.PIPE)
+    assert r.returncode == 0, f"failed to find yosys {r}"
+    btor_name = filename.stem + ".btor"
+    yosys_cmd = f"read_verilog {filename.name} ; proc ; write_btor -x {btor_name}"
+    subprocess.run(["yosys", "-p", yosys_cmd], check=True, cwd=cwd, stdout=subprocess.PIPE)
+    assert (cwd / btor_name).exists()
+    return cwd / btor_name
+
 
 
 def main():
-    filename = parse_args()
+    filename, working_dir = parse_args()
+    create_working_dir(working_dir)
     ast = parse_verilog(filename)
     replace_literals(ast)
-    print(serialize(ast))
+    synth_filename = working_dir / filename.name
+    with open(synth_filename, "w") as f:
+        f.write(serialize(ast))
+    btor_filename = to_btor(synth_filename)
+    print(btor_filename)
 
 
 def parse_verilog(filename: Path) -> Source:
