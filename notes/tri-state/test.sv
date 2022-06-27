8a34ed1a4c71d021c24255d14645cc76c366062d
Kevin Läufer <laeufer@cs.berkeley.edu>
2022-06-27 15:21:21 -0700

    tristate notes

--- notes/tri-state/test.sv
+++ notes/tri-state/test.sv
@@ -1,6 +1,10 @@
 // yosys read_verilog:
 // >> Warning: Yosys has only limited support for tri-state logic at the moment. (test.sv:5)
 // the btor and smt produced by yosys are both not very helpful :(
+//
+// according to this question on reddit, there might be a command to emulate tristate logic:
+// https://www.reddit.com/r/yosys/comments/60yqrq/limited_support_for_tristate_logic/
+//
 module test(input read, inout io, output out);
 
 assign out = io;
