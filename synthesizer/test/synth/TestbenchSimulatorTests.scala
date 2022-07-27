6c5e2c174e66cd71a51df5ea9507bec66a74a34a
Kevin Läufer <laeufer@cs.berkeley.edu>
2022-07-27 13:49:39 -0400

    add missing tests

--- synthesizer/test/synth/TestbenchSimulatorTests.scala
+++ synthesizer/test/synth/TestbenchSimulatorTests.scala
@@ -4,7 +4,7 @@
 
 package synth
 
-import maltese.mc.Btor2
+import maltese.mc.{Btor2, IsOutput, TransitionSystem, TransitionSystemSim}
 import org.scalatest.flatspec.AnyFlatSpec
 import synth.Synthesizer.initSys
 
@@ -15,6 +15,25 @@ class TestbenchSimulatorTests extends AnyFlatSpec {
   val CirFixDir = os.pwd / os.up / "benchmarks" / "cirfix"
 
   it should "correctly execute i2c master testbench" in {
+    val tb = Testbench.load(CirFixDir / "opencores" / "i2c" / "fixed_x_prop_tb.csv")
+    assert(tb.length == 171957)
+
+    val sys = Btor2.load(BenchmarkDir / "i2c_master.btor")
+
+    // pick random starting state
+    val seed: Long = 1
+    val rnd = new scala.util.Random(seed)
+    val initialized = initSys(sys, RandomInit, rnd)
+
+    // pick random inputs for Xs
+    val randInputTb = Testbench.addRandomInput(sys, tb, rnd)
+
+    val r = Testbench.run(initialized, randInputTb, verbose = true)
+    assert(!r.failed)
+  }
+
+  // this is the code we used to generate the x-prop fix
+  it should "fix x-prop issues with the i2c testbench" in {
     val tb = Testbench.load(CirFixDir / "opencores" / "i2c" / "orig_tb.csv")
     assert(tb.length == 171957)
 
@@ -25,8 +44,42 @@ class TestbenchSimulatorTests extends AnyFlatSpec {
     val rnd = new scala.util.Random(seed)
     val initialized = initSys(sys, RandomInit, rnd)
 
-    val r = Testbench.run(initialized, tb, verbose = true)
-    assert(!r.failed)
+    // fix X-prop issues
+    val fixed = XInputConversion(initialized, tb, rnd, verbose = false)
+
+    Testbench.save(CirFixDir / "opencores" / "i2c" / "fixed_x_prop_tb.csv", fixed)
   }
 
+}
+
+private object XInputConversion {
+
+  /** Generates random inputs for all X-inputs and corrects specified (non-X) output where necessary.
+    * The system needs to be the original, ground truth system without a bug.
+    */
+  def apply(sys: TransitionSystem, tb: Testbench, rnd: scala.util.Random, verbose: Boolean): Testbench = {
+    // pick random inputs for Xs
+    val randInputTb = Testbench.addRandomInput(sys, tb, rnd)
+
+    // run the testbench
+    val r = Testbench.run(sys, randInputTb, verbose = false)
+
+    if (!r.failed) { return randInputTb }
+
+    // analyze the failed outputs and patch them up
+    val isOutput = sys.signals.filter(_.lbl == IsOutput).map(_.name).toSet
+    val values = randInputTb.values.zip(r.values).zipWithIndex.map { case ((tbValues, rValues), ii) =>
+      tbValues.zip(randInputTb.signals).map {
+        case (Some(expected), name) if isOutput(name) =>
+          val actual = rValues(name)
+          if (expected != actual) {
+            if (verbose) println(s"$name@$ii: $expected -> $actual")
+          }
+          Some(actual)
+        case (other, _) => other
+      }
+    }
+
+    randInputTb.copy(values = values)
+  }
 }
