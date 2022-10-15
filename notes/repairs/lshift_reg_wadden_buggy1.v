f9f8d2cb3a19bccbd26ebb279840e3d874725637
Kevin Läufer <laeufer@cs.berkeley.edu>
2022-10-14 21:02:42 -0400

    add more repaired files

---
+++ notes/repairs/lshift_reg_wadden_buggy1.v
@@ -0,0 +1,28 @@
+module lshift_reg
+(
+  input clk,
+  input rstn,
+  input [7:0] load_val,
+  input load_en,
+  output reg [7:0] op
+);
+
+  integer i;
+
+  always @(posedge clk) begin
+    if(!rstn) begin
+      op = 0;
+    end else begin
+      if(load_en) begin
+        op = load_val;
+      end else begin
+        for(i=0; i<8; i=i+1) begin
+          op[i + 1] = op[i];
+        end
+        op[0] = op[7];
+      end
+    end
+  end
+
+
+endmodule
