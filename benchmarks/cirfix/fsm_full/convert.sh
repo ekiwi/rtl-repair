2d2509e52559c3458b843d7b4ae0a2b033f2f7c3
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-02-22 12:49:17 -0500

    fsm full: update

--- benchmarks/cirfix/fsm_full/convert.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-yosys -p "read_verilog fsm_full.v ; proc -noopt ; write_btor -x fsm_full.btor"
-yosys -p "read_verilog fsm_full_wadden_buggy1.v ; proc -noopt ; write_btor -x fsm_full_wadden_buggy1.btor"
-yosys -p "read_verilog fsm_full_ssscrazy_buggy2.v ; proc -noopt ; write_btor -x fsm_full_ssscrazy_buggy2.btor"
-
-
