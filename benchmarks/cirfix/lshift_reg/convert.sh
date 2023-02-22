585ec109305cfe05a51be4011dbb3321698b4f4f
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-02-22 12:50:42 -0500

    shift reg: update

--- benchmarks/cirfix/lshift_reg/convert.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-yosys -p "read_verilog lshift_reg.v ; proc -noopt ; write_btor -x lshift_reg.btor"
-yosys -p "read_verilog lshift_reg_wadden_buggy1.v ; proc -noopt ; write_btor -x lshift_reg_wadden_buggy1.btor"
-yosys -p "read_verilog lshift_reg_wadden_buggy2.v ; proc -noopt ; write_btor -x lshift_reg_wadden_buggy2.btor"
-
