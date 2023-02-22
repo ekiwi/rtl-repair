dc1a2adf736ea19ac611589a24b770286f82f3c3
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-02-22 12:47:14 -0500

    flip flop update

--- benchmarks/cirfix/flip_flop/convert.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-yosys -p "read_verilog tff.v ; proc -noopt ; write_btor -x tff.btor"
-yosys -p "read_verilog tff_wadden_buggy1.v ; proc -noopt ; write_btor -x tff_wadden_buggy1.btor"
-yosys -p "read_verilog tff_wadden_buggy2.v ; proc -noopt ; write_btor -x tff_wadden_buggy2.btor"
-
