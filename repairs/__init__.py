6d106ba948c5968437030e634edf1bbedd934591
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-04-19 16:11:21 -0400

    improve repair checks

--- repairs/__init__.py
+++ repairs/__init__.py
@@ -7,7 +7,8 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-from benchmarks import Benchmark, get_other_sources, VerilogOracleTestbench
+from benchmarks import Benchmark, get_other_sources, VerilogOracleTestbench, load_project, get_benchmark, \
+    _assert_file_exists, _assert_dir_exists
 from benchmarks.yosys import to_gatelevel_netlist
 from benchmarks.run import run, RunConf
 
@@ -27,37 +28,108 @@ repairs = {
     "cirfix": cirfix_repairs
 }
 
+def parse_csv_line(line: str) -> list:
+    return [n.strip() for n in line.split(',')]
+
+@dataclass
+class SimResult:
+    no_output: bool = False
+    failed_at: int = -1
+    fail_msg: str = ""
+    @property
+    def is_success(self): return self.failed_at == -1 and not self.no_output
+
+def check_against_oracle(oracle_filename: Path, output_filename: Path):
+    with open(oracle_filename) as oracle, open(output_filename) as output:
+        oracle_header, output_header = parse_csv_line(oracle.readline()), parse_csv_line(output.readline())
+        assert oracle_header == output_header, f"{oracle_header} != {output_header}"
+        header = oracle_header[1:]
+
+        # compare line by line
+        for (ii, (expected, actual)) in enumerate(zip(oracle, output)):
+            expected, actual = parse_csv_line(expected), parse_csv_line(actual)
+            # remove first line (time)
+            expected, actual = expected[1:], actual[1:]
+            correct = expected == actual
+            if not correct:
+                msg = []
+                for ee, aa, nn in zip(expected, actual, header):
+                    if ee != aa:
+                        msg.append(f"{nn}@{ii}: {aa} != {ee} (expected)")
+                return SimResult(failed_at=ii, fail_msg='\n'.join(msg))
+
+        # are we missing some output?
+        remaining_oracle_lines = oracle.readlines()
+        if len(remaining_oracle_lines) > 0:
+            # we expected more output => fail!
+            msg = f"Output stopped at {ii}. Expected {len(remaining_oracle_lines)} more lines."
+            return SimResult(failed_at=ii, fail_msg=msg)
+
+    return SimResult()
+
+
+
 def check_gatelevel_sim(working_dir: Path, benchmark: Benchmark, repaired: Path):
-    assert working_dir.exists(), f"Does not exist: {working_dir.resolve()}"
     other_sources = get_other_sources(benchmark)
     # synthesize
     gate_level = working_dir / f"{repaired.stem}.gatelevel.v"
-    to_gatelevel_netlist(working_dir, gate_level, [repaired] + other_sources, top=benchmark.project.top)
+    to_gatelevel_netlist(working_dir, gate_level, [repaired] + other_sources, top=benchmark.project.design.top)
+    # check
+    return check_sim(working_dir, benchmark, [gate_level] + other_sources)
+
+def check_sim(working_dir: Path, benchmark: Benchmark, design_sources: list):
     # run testbench
     assert isinstance(benchmark.testbench, VerilogOracleTestbench)
-    tb_sources = [gate_level] + other_sources + benchmark.testbench.sources
-    run(working_dir, 'iverilog', tb_sources, RunConf(verbose=True, show_stdout=True))
-    # TODO: actually check the output of the testbench!
+    tb_sources = design_sources + benchmark.testbench.sources
+    conf = RunConf(include_dir=benchmark.project.design.directory, verbose=False, show_stdout=False)
+    run(working_dir, 'iverilog', tb_sources, conf)
+
+    # check the output
+    output = working_dir / benchmark.testbench.output
+    if not output.exists():
+        # no output was produced --> fail
+        msg = "No output was produced."
+        return SimResult(no_output=True, fail_msg=msg)
+    else:
+        return check_against_oracle(benchmark.testbench.oracle, output)
+
+
+
+def check_repair(working_dir: Path, project_file: Path, bug_name: str, repaired: Path):
+    _assert_file_exists("repaired Verilog", repaired)
+    _assert_dir_exists("working directory", working_dir)
+    project = load_project(project_file)
+    benchmark = get_benchmark(project, bug_name)
+
+    # first we just simulate and check the oracle
+    other_sources = get_other_sources(benchmark)
+    sim_res = check_sim(working_dir, benchmark, [repaired] + other_sources)
+    if not sim_res.is_success:
+        print(f"RTL-sim: {sim_res}")
+
+    # now we do the gate-level sim, do we get the same result?
+    gate_res = check_gatelevel_sim(working_dir, benchmark, repaired)
+    print(f"Gate-level: {gate_res}")
 
 
 
 def _simple_test():
     # cirfix solution
     root_dir = Path(__file__).parent.parent.resolve()
-    repaired = root_dir / "notes" / "repairs" / "mux_4_1_wadden_buggy1.cirfix.v"
-    assert repaired.exists()
 
-    # benchmark description
-    from benchmarks import load_benchmark_by_name
-    bench = load_benchmark_by_name("mux_4_1", "kgoliya_buggy1")
+    repaired = root_dir / "notes" / "repairs" / "mux_4_1_wadden_buggy1.cirfix.v"
+    project = "mux_4_1"
+    bug = "kgoliya_buggy1"
 
     # working dir
     working_dir = root_dir / "tmp"
     if not working_dir.exists():
         working_dir.mkdir()
 
-    # check
-    check_gatelevel_sim(working_dir, bench, repaired)
+    # benchmark description
+    from benchmarks import projects
+    proj = projects[project]
+    check_repair(working_dir, proj, bug, repaired)
 
 if __name__ == '__main__':
     _simple_test()
