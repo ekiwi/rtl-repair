669b572e1a62c59e4223c3caa1a2a7984746cd03
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-05-15 15:14:44 -0400

    wip: demo

---
+++ demo/project/first_counter.v
@@ -0,0 +1,19 @@
+module first_counter(
+    input clock,
+    input reset,
+    input enable,
+    output reg [3:0] count,
+    output reg overflow
+);
+always@(posedge clock) begin
+  if(reset == 1'b1) begin
+    overflow <= 1'b0;
+    count <= 4'b0;
+  end else if(enable == 1'b1) begin
+    count <= count + 1;
+  end
+  if(count == 4'b1111) begin
+    overflow <= 1'b1; // comment
+  end
+end
+endmodule
---
+++ demo/run_tb.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Copyright 2023 The Regents of the University of California
+# released under BSD 3-Clause License
+# author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+#
+# runs the testbench with iverilog, producing a VCD
