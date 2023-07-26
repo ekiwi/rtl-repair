c774c23f159855b3f07878fcc9ccc2a7fdac9a0e
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-07-26 12:17:44 -0400

    Revert "benchmarks: try to create cirfix oracle compatible output"
    This reverts commit c5d939ab232375860374cbccb9177e2f285570ee.

--- benchmarks/cirfix/sdram_controller/sdram_controller_tb_t1.no_tri_state.v
+++ benchmarks/cirfix/sdram_controller/sdram_controller_tb_t1.no_tri_state.v
@@ -4,11 +4,6 @@
  *  - Write
  *  - Read
  */
-
-// enable cirfix oracle compatible output, this is required by the check_results.py script
- `define CIRFIX_OUTPUT
-
-
 module sdram_controller_tb();
 
     /*vlog_tb_utils vlog_tb_utils0();*/
@@ -58,25 +53,16 @@ module sdram_controller_tb();
 
     initial begin
         f = $fopen("output_sdram_controller_tb_t1.txt");
-`ifdef CIRFIX_OUTPUT
-        $fwrite(f, "time,rd_data[15],rd_data[14],rd_data[13],rd_data[12],rd_data[11],rd_data[10],rd_data[9],rd_data[8],rd_data[7],rd_data[6],rd_data[5],rd_data[4],rd_data[3],rd_data[2],rd_data[1],rd_data[0],rd_ready,addr[12],addr[11],addr[10],addr[9],addr[8],addr[7],addr[6],addr[5],addr[4],addr[3],addr[2],addr[1],addr[0],bank_addr[1],bank_addr[0],data[15],data[14],data[13],data[12],data[11],data[10],data[9],data[8],data[7],data[6],data[5],data[4],data[3],data[2],data[1],data[0],clock_enable,cs_n,ras_n,cas_n,we_n,data_mask_low,data_mask_high\n");
-`else
         // inputs
         $fwrite(f, "time,wr_addr,wr_data,wr_enable,rd_addr,rd_enable,rst_n,clk,data_in,");
         // outputs
         $fwrite(f, "rd_data,rd_ready,busy,addr,bank_addr,data_oe,data_out,clock_enable,cs_n,ras_n,cas_n,we_n,data_mask_low,data_mask_high\n");
-`endif
 	forever begin
 	    @(posedge clk);
-`ifdef CIRFIX_OUTPUT
-        $fwrite(f,"%g,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b\n",
-	    $time,data_output[15],data_output[14],data_output[13],data_output[12],data_output[11],data_output[10],data_output[9],data_output[8],data_output[7],data_output[6],data_output[5],data_output[4],data_output[3],data_output[2],data_output[1],data_output[0],rd_ready,addr[12],addr[11],addr[10],addr[9],addr[8],addr[7],addr[6],addr[5],addr[4],addr[3],addr[2],addr[1],addr[0],bank_addr[1],bank_addr[0],data[15],data[14],data[13],data[12],data[11],data[10],data[9],data[8],data[7],data[6],data[5],data[4],data[3],data[2],data[1],data[0],clock_enable,cs_n,ras_n,cas_n,we_n,data_mask_low,data_mask_high);
-`else
        // inputs
         $fwrite(f,"%g,%d,%d,%d,%d,%d,%d,%d,%d,", $time, haddr,data_input,wr_enable,rd_addr,rd_enable,rst_n,clk,data_in);
         // outputs
         $fwrite(f,"%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n", data_output,rd_ready,busy,addr,bank_addr,data_oe,data_out,clock_enable,cs_n,ras_n,cas_n,we_n,data_mask_low,data_mask_high);
-`endif
 	end
     end
 
