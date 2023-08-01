91451780c6c5c89e8e0ef8793a652dd59eda6a74
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-08-01 12:20:35 -0400

    calc osdd: write computer readable output

---
+++ scripts/create_tables.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Copyright 2023 The Regents of the University of California
+# released under BSD 3-Clause License
+# author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+#
+# creates the tables for the evaluation section of the RTL-Repair paper
+
