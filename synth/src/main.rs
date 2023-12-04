4e1ae80938ebe830c165ed37d3d686aa484409e3
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-12-04 16:01:18 -0500

    wip: re-implement synthesizer in rust

---
+++ synth/src/main.rs
@@ -0,0 +1,65 @@
+// Copyright 2023 The Regents of the University of California
+// released under BSD 3-Clause License
+// author: Kevin Laeufer <laeufer@berkeley.edu>
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(name = "synth")]
+#[command(author = "Kevin Laeufer <laeufer@berkeley.edu>")]
+#[command(version)]
+#[command(about = "Generates repair solutions.", long_about = None)]
+struct Args {
+    #[arg(
+        long,
+        required = true,
+        help = "the design to be repaired with the template instantiated in btor format"
+    )]
+    design: String,
+    #[arg(long, required = true, help = "the testbench in CSV format")]
+    testbench: String,
+    #[arg(long, help = "output debug messages")]
+    verbose: bool,
+    #[arg(long, help = "use the incremental instead of the basic synthesizer")]
+    incremental: bool,
+    #[arg(
+        long,
+        value_enum,
+        default_value = "bitwuzla",
+        help = "the SMT solver to use"
+    )]
+    solver: Solver,
+    #[arg(
+        long,
+        value_enum,
+        default_value = "zero",
+        help = "initialization strategy"
+    )]
+    init: Init,
+    #[arg(
+        long,
+        default_value_t = 2u64,
+        help = "step size for past-k in incremental solver"
+    )]
+    pask_k_step_size: u64,
+    #[arg(
+        long,
+        default_value_t = 32u64,
+        help = "the maximum repair window size before the incremental solver gives up"
+    )]
+    max_repair_window_size: u64,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Solver {
+    Bitwuzla,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Init {
+    Zero,
+    Random,
+}
+
+fn main() {
+    let args = Args::parse();
+}
