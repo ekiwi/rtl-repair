761764f724c6446a81f80561fc6f99658510eede
Kevin Läufer <laeufer@cs.berkeley.edu>
2024-01-04 15:42:48 -0500

    update copyright a bit

--- rtlrepair.py
+++ rtlrepair.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2022 The Regents of the University of California
+# Copyright 2022-2024 The Regents of the University of California
 # released under BSD 3-Clause License
 # author: Kevin Laeufer <laeufer@cs.berkeley.edu>
 
