67cca6c6d4ff61d9942a594d24409c9b6f751841
Kevin Läufer <laeufer@cs.berkeley.edu>
2024-01-05 15:17:17 -0500

    bench: c4 initialize vars to zero in order to get same behavior in Verilator and iVerilog

--- benchmarks/fpga-debugging/axis-async-fifo-c4/axis_register.v
+++ benchmarks/fpga-debugging/axis-async-fifo-c4/axis_register.v
@@ -95,7 +95,7 @@ if (REG_TYPE > 1) begin
     reg [DATA_WIDTH-1:0] m_axis_tdata_reg  = {DATA_WIDTH{1'b0}};
     reg [KEEP_WIDTH-1:0] m_axis_tkeep_reg  = {KEEP_WIDTH{1'b0}};
     reg                  m_axis_tvalid_reg = 1'b0;
-    reg                  m_axis_tvalid_next;
+    reg                  m_axis_tvalid_next = 0;
     reg                  m_axis_tlast_reg  = 1'b0;
     reg [ID_WIDTH-1:0]   m_axis_tid_reg    = {ID_WIDTH{1'b0}};
     reg [DEST_WIDTH-1:0] m_axis_tdest_reg  = {DEST_WIDTH{1'b0}};
@@ -104,16 +104,16 @@ if (REG_TYPE > 1) begin
     reg [DATA_WIDTH-1:0] temp_m_axis_tdata_reg  = {DATA_WIDTH{1'b0}};
     reg [KEEP_WIDTH-1:0] temp_m_axis_tkeep_reg  = {KEEP_WIDTH{1'b0}};
     reg                  temp_m_axis_tvalid_reg = 1'b0;
-    reg                  temp_m_axis_tvalid_next;
+    reg                  temp_m_axis_tvalid_next = 0;
     reg                  temp_m_axis_tlast_reg  = 1'b0;
     reg [ID_WIDTH-1:0]   temp_m_axis_tid_reg    = {ID_WIDTH{1'b0}};
     reg [DEST_WIDTH-1:0] temp_m_axis_tdest_reg  = {DEST_WIDTH{1'b0}};
     reg [USER_WIDTH-1:0] temp_m_axis_tuser_reg  = {USER_WIDTH{1'b0}};
 
     // datapath control
-    reg store_axis_input_to_output;
-    reg store_axis_input_to_temp;
-    reg store_axis_temp_to_output;
+    reg store_axis_input_to_output = 0;
+    reg store_axis_input_to_temp = 0;
+    reg store_axis_temp_to_output = 0;
 
     assign s_axis_tready = s_axis_tready_reg;
 
