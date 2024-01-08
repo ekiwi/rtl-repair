93ea75405bbf8e4209cdbe2b86aa20bc5f07127f
Kevin Läufer <laeufer@cs.berkeley.edu>
2024-01-08 11:22:26 -0500

    bench: minimize changes between original and buggy d13

--- benchmarks/fpga-debugging/axis-frame-len-d13/axis_frame_len.v
+++ benchmarks/fpga-debugging/axis-frame-len-d13/axis_frame_len.v
@@ -64,6 +64,8 @@ reg [LEN_WIDTH-1:0] frame_len_reg = 0;
 reg [LEN_WIDTH-1:0] frame_len_next;
 reg frame_len_valid_reg = 1'b0;
 reg frame_len_valid_next;
+reg frame_reg = 1'b0;
+reg frame_next;
 
 assign frame_len = frame_len_reg;
 assign frame_len_valid = frame_len_valid_reg;
@@ -73,6 +75,7 @@ integer offset, i, bit_cnt;
 always @* begin
     frame_len_next = frame_len_reg;
     frame_len_valid_next = 1'b0;
+    frame_next = frame_reg;
 
     if (frame_len_valid_reg) begin
         frame_len_next = 0;
@@ -84,6 +87,10 @@ always @* begin
         if (monitor_axis_tlast) begin
             // end of frame
             frame_len_valid_next = 1'b1;
+            frame_next = 1'b0;
+        end else if (!frame_reg) begin
+            // first word after end of frame
+            frame_next = 1'b1;
         end
 
         // increment frame length by number of words transferred
@@ -103,9 +110,11 @@ always @(posedge clk) begin
     if (rst) begin
         frame_len_reg <= 0;
         frame_len_valid_reg <= 0;
+        frame_reg <= 1'b0;
     end else begin
         frame_len_reg <= frame_len_next;
         frame_len_valid_reg <= frame_len_valid_next;
+        frame_reg <= frame_next;
     end
 end
 
