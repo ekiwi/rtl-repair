784d20a89c37d9aef34308b7e97bd6b6268c5c54
Kevin Läufer <laeufer@cs.berkeley.edu>
2024-03-08 09:19:34 -0500

    sha3 is a cirfix benchmark

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -393,7 +393,7 @@ def is_cirfix_paper_benchmark(benchmark: Benchmark) -> bool:
             benchmark.bug.name in cirfix_seeds[benchmark.project_name])
 
 def is_cirfix_paper_project(project: Project) -> bool:
-    return project.name in cirfix_seeds
+    return project.name in cirfix_seeds or project.name == 'sha3_padder'
 
 def is_fpga_debugging_benchmark(benchmark: Benchmark) -> bool:
     return benchmark.project_name in fpga_projects
