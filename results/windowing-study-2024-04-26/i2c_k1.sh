ff55c70f990feb664d55114162f71a824213264d
Kevin Läufer <laeufer@cs.berkeley.edu>
2024-04-17 15:41:28 -0400

    wip: windowing study

---
+++ results/windowing-study-2024-04-26/i2c_k1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+../synth/target/release/synth \
+  --design ../working-dir/i2c_master_kgoliya_buggy1_fixed_x_prop_tb/3_conditional_overwrite/i2c_master_bit_ctrl_kgoliya_buggy1.sync_reset.instrumented.btor \
+  --testbench ../benchmarks/cirfix/opencores/i2c/fixed_x_prop_tb.csv \
+  --solver bitwuzla --init any --windowing --max-repair-window-size 512 > i2c_k1.log
+
