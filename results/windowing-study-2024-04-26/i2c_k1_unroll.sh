cdf7ae36105b98cb8a11049a3e532f2142715ce1
Kevin Läufer <laeufer@cs.berkeley.edu>
2024-04-18 10:47:38 -0400

    windowing study: update shell scripts

--- results/windowing-study-2024-04-26/i2c_k1_unroll.sh
+++ results/windowing-study-2024-04-26/i2c_k1_unroll.sh
@@ -3,5 +3,5 @@
 ../../synth/target/release/synth \
   --design ../../working-dir/i2c_master_kgoliya_buggy1_fixed_x_prop_tb/3_conditional_overwrite/i2c_master_bit_ctrl_kgoliya_buggy1.sync_reset.instrumented.btor \
   --testbench ../../benchmarks/cirfix/opencores/i2c/fixed_x_prop_tb.csv \
-  --solver bitwuzla --init any --study-windowing --max-repair-window-size 512 > i2c_k1.log
+  --solver bitwuzla --init any --study-unrolling > i2c_k1_unroll.log
 
--- results/windowing-study-2024-04-26/sdram_w2_unroll.sh
+++ results/windowing-study-2024-04-26/sdram_w2_unroll.sh
@@ -4,5 +4,5 @@
 ../../synth/target/release/synth \
   --design ../../working-dir/sdram_controller_wadden_buggy2_oracle-full/1_replace_literals/sdram_controller_wadden_buggy2.no_tri_state.instrumented.btor \
   --testbench ../../benchmarks/cirfix/sdram_controller/orig_tb.csv \
-  --solver bitwuzla --init any --study-windowing --max-repair-window-size 512 > sdram_w2.log
+  --solver bitwuzla --init any --study-unrolling > sdram_w2_unroll.log
 
