9cd043ebf4125da090ec29d94edc28a23a042e1e
Kevin Läufer <laeufer@cs.berkeley.edu>
2024-04-18 10:47:38 -0400

    windowing study: update shell scripts

--- results/windowing-study-2024-04-26/c1_unroll.sh
+++ results/windowing-study-2024-04-26/c1_unroll.sh
@@ -4,5 +4,5 @@
 ../../synth/target/release/synth \
   --design ../../working-dir/zipcpu-spi-c1-c3-d9_c1_csv/2_add_guard/llsdspi_bug_c1.instrumented.btor \
   --testbench ../../benchmarks/fpga-debugging/zipcpu-spi-c1-c3-d9/tb.csv \
-  --solver bitwuzla --init zero --study-windowing --max-repair-window-size 512 > c1.log
+  --solver bitwuzla --init zero --study-unrolling > c1_unroll.log
 
--- results/windowing-study-2024-04-26/sdram_w1_unroll.sh
+++ results/windowing-study-2024-04-26/sdram_w1_unroll.sh
@@ -4,5 +4,5 @@
 ../../synth/target/release/synth \
   --design ../../working-dir/sdram_controller_wadden_buggy1_oracle-full/3_conditional_overwrite/sdram_controller_wadden_buggy1.no_tri_state.instrumented.btor \
   --testbench ../../benchmarks/cirfix/sdram_controller/orig_tb.csv \
-  --solver bitwuzla --init any --study-windowing --max-repair-window-size 512 > sdram_w1.log
+  --solver bitwuzla --init any --study-unrolling > sdram_w1_unroll.log
 
