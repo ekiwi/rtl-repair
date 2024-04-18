b45bfa22b1b0ebae51c43a799f2563800efe71d3
Kevin Läufer <laeufer@cs.berkeley.edu>
2024-04-18 09:29:10 -0400

    windowing: change stats format

---
+++ synth/src/studies/unrolling.rs
@@ -0,0 +1,5 @@
+// Copyright 2024 The Regents of the University of California
+// released under BSD 3-Clause License
+// author: Kevin Laeufer <laeufer@berkeley.edu>
+//
+// we study how the length of unrolling affects the basic synthesis approach
